88e34e94a67cf29d80892bbe11ba4783aace78f4
Lei Sun <sunray0620@gmail.com>
2024-10-27 04:22:16 +0000

    Add my first test

--- audiointro/tests.py
+++ audiointro/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class AudioIntroViewsTests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_index_view(self):
+        url = reverse('index', kwargs={})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        # self.assertTemplateUsed(response, 'myapp/book_detail.html')
+        self.assertContains(response, "audiointro")
